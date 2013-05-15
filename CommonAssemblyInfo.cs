d2db7de93cf0a4900d7a8798700b6be1df8ebee3
Damian Reeves <DamReev@gmail.com>
2013-05-14 22:42:56 -0400

    Version bump

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -2,6 +2,6 @@
 
 [assembly: AssemblyTitle("Commander")]
 [assembly: AssemblyProduct("Commander")]
-[assembly: AssemblyVersion("0.0.1.0")]
-[assembly: AssemblyFileVersion("0.0.1.0")]
+[assembly: AssemblyVersion("0.0.2.0")]
+[assembly: AssemblyFileVersion("0.0.2.0")]
 [assembly:AssemblyInformationalVersion("0.0.1-alpha")]
