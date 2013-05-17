d06709559885daae7dbe05e66e0e305ab0f26ce4
Damian Reeves <DamReev@gmail.com>
2013-05-17 07:29:42 -0400

    Add Portable Commander library with default attribute implementations and add a test assembly for win phone 8

---
+++ Commander/OnCommandAttribute.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Commander
+{
+    [AttributeUsage(AttributeTargets.Method, AllowMultiple = true, Inherited = true)]
+    public class OnCommandAttribute : Attribute
+    {
+        private readonly string _commandName;
+
+        public OnCommandAttribute(string commandName)
+        {
+            if (commandName == null)
+            {
+                throw new ArgumentNullException("commandName");
+            }
+            _commandName = commandName;
+        }
+
+        public string CommandName
+        {
+            get { return _commandName; }
+        }
+
+        //public Type CommandImplementationType { get; set; }
+    }
+}
---
+++ Commander/OnCommandCanExecuteAttribute.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Commander
+{
+    [AttributeUsage(AttributeTargets.Method, AllowMultiple = true, Inherited = true)]
+    public class OnCommandCanExecuteAttribute : Attribute
+    {
+        private readonly string _commandName;
+
+        public OnCommandCanExecuteAttribute(string commandName)
+        {
+            if (commandName == null)
+            {
+                throw new ArgumentNullException("commandName");
+            }
+            _commandName = commandName;
+        }
+
+        public string CommandName
+        {
+            get { return _commandName; }
+        }
+    }
+}
